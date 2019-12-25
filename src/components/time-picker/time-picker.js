e3e7dc5f9c435f8886d1da87ce0324e779393f94
FeltBoots <donskoi.ia@stundents.dvfu.ru>
2019-12-26 01:16:01 +1000

    TimePicker: init

---
+++ src/components/time-picker/time-picker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TimePickerContainer extends Component {
+  render() {
+    return (
+      <div>
+        Time Picker with selects
+      </div>
+    )
+  }
+}
+
+export default TimePickerContainer;
