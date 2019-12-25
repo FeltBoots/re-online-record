a1a727fa256b357263a3cd2bc7710023f7a4d46d
FeltBoots <donskoi.ia@stundents.dvfu.ru>
2019-12-25 23:56:05 +1000

    Reducers: Init

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,38 @@
-const reducer = (state, action) => {
-  return {
+const initialState = {
+  cities: [],
+  time: [],
+  loading: true,
+  error: null,
+};
 
-  };
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_CITIES_REQUEST':
+      return {
+        cities: [],
+        time: [],
+        loading: true,
+        error: null,
+      };
+    case 'FETCH_CITIES_SUCCESS':
+      return {
+        cities: action.payload,
+        time: [],
+        loading: false,
+        error: null,
+      };
+    case 'FETCH_CITIES_FAILURE':
+      return {
+        cities: [],
+        time: [],
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
 };
 
 export default reducer;
