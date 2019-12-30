2564eaf849d7d03e7fec74b40bad6264ec4556ae
FeltBoots <donskoi.ia@stundents.dvfu.ru>
2019-12-31 00:56:30 +1000

    NameInput: Add validation styles

--- src/components/name-input/name-input.js
+++ src/components/name-input/name-input.js
@@ -3,12 +3,14 @@ import { selectName, validateName } from "../../actions";
 import { connect } from "react-redux";
 import ValidationError from "../validation-error";
 import { bindActionCreators } from "redux";
+import '../app/app.css';
 
-const NameInput = ({ selectName, nameError, validateName}) => {
+const NameInput = ({ selectName, nameError, validateName }) => {
+  const errorClass = nameError ? 'error' : null;
   return (
     <Fragment>
       <input
-        className="input"
+        className={`input ${errorClass}`}
         type="text"
         placeholder="Ваше имя"
         onBlur={(e) => {
